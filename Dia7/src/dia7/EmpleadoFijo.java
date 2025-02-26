7275cfca3e4361c3cfa928a244cadfe76c112e23
MariaGLi <lizarazoleal27@gmail.com>
2025-02-26 10:14:33 -0500

    :sparkles: Herencia y Polimorfismo Java.

--- Dia7/src/dia7/EmpleadoFijo.java
+++ Dia7/src/dia7/EmpleadoFijo.java
@@ -1,5 +1,12 @@
 package dia7;
 
-public class EmpleadoFijo {
+public class EmpleadoFijo extends GestionEmpleados {
+
+    public EmpleadoFijo() {
+    }
+
+    public EmpleadoFijo(String nombre, double salario) {
+        super(nombre, salario);
+    }
     
 }
--- Dia7/src/dia7/GestionEmpleados.java
+++ Dia7/src/dia7/GestionEmpleados.java
@@ -1,5 +1,15 @@
 package dia7;
 
 public class GestionEmpleados {
+    protected String nombre;
+    protected double salario;
+
+    public GestionEmpleados() {
+    }
+
+    public GestionEmpleados(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
     
 }
